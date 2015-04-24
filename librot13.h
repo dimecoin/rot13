67a637d9dcc8b7ce34ae10e275103b0237734db9
dimecoin <dimecoin@gmail.com>
2015-04-24 13:02:01 -0500

    rollover shouldn't be extern

--- librot13.h
+++ librot13.h
@@ -4,6 +4,6 @@
 #define ROT13_VERSION_STRING  "0.4"
 
 extern void rot13 (char *input, char *output, bool useRot5);
-extern int rollover(int min, int max, int step, int input);
+int rollover(int min, int max, int step, int input);
 
 #endif
